822994a41fd27956d3e67497103fa3737fb66ef5
Farhana <55168133+farhanarahman2299@users.noreply.github.com>
2020-03-05 13:57:31 +0600

    Add files via upload

--- Lab33.java
+++ Lab33.java
@@ -1,13 +1,22 @@
-
 package lab3.pkg3;
+
+
 
 public class Lab33 {
 
-    
     public static void main(String[] args) {
-        // TODO code application logic here
-        String s = "BANGLADESH";
-        System.out.println(s.toUpperCase());
+        int sum =0;
+        int arr[];
+        arr = new int[4];
+        arr[0] = 95;
+        arr[1] = 127;
+        arr[2] = 2;
+        arr[3] = 1885;
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Index["+i+"]= "+arr[i]);
+            sum = sum + arr[i];
+        }
+        System.out.println("Array sum: "+sum);
+        
     }
-    
 }
