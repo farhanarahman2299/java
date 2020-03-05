31f8ef8607e00d00d3cf3d3f5df140e9b411341f
Farhana <55168133+farhanarahman2299@users.noreply.github.com>
2020-03-05 13:32:47 +0600

    Add files via upload

---
+++ Lab33.java
@@ -0,0 +1,13 @@
+
+package lab3.pkg3;
+
+public class Lab33 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s = "BANGLADESH";
+        System.out.println(s.toUpperCase());
+    }
+    
+}
