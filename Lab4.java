787e728a3f50c4c2398b5a94a035af5d0f530171
Farhana <55168133+farhanarahman2299@users.noreply.github.com>
2020-03-12 13:04:18 +0600

    Add files via upload

--- Lab4.java
+++ Lab4.java
@@ -6,8 +6,15 @@ public class Lab4 {
 
   
     public static void main(String[] args) {
-       Nesting n=new Nesting(5,10);
-       n.display();
-    }
+//       Nesting n=new Nesting(5,10);
+//       n.display();
+        String s ="Hello word";
+        String t = s.toUpperCase();
+        System.out.println(t);
+         String u= s.toLowerCase();
+          System.out.println(u);
+           System.out.println(s.length());
+            System.out.println(s+t+u);
+  }
     
 }
